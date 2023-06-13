447d66c3c9a8dfed035af8df9fd8d7e5c9579e03
JackGoodings <jackgoodings@icloud.com>
2023-06-13 12:17:12 +0100

    day 1

---
+++ Day1practice/Day1practice/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//string variable
+
+string myName = "Sara";
+
+//creating another variable
+
+string superHeroName = "The Flash";
+
+Console.WriteLine(superHeroName);
+
+//building the output string
+
+string outputString = "This thursday, I will be watching the movie, " + superHeroName;
+Console.WriteLine(outputString);
+
+// create a variable to store an intiger
+
+int myAge = 40;
+
+Console.WriteLine(myAge);
+
+//create a decimal variable
+
+decimal myMoney = 100.50m;
+
+Console.WriteLine(myMoney);
+
+//
