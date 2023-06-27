17cb12b09cc75e40a6bcf60fafa37c0b295d804e
JackGoodings <jackgoodings@icloud.com>
2023-06-27 12:05:59 +0100

    Day 5 Student Developer

---
+++ StudentDeveloper/StudentDeveloper/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDeveloper
+{
+    public class Student
+    {
+        public int StudyHoursPerWeek { get; set; }
+        public int StudyWeeksCompleted { get; set; }
+        public string StudentName { get; set; }
+
+        //for the sake of the output
+        public string OutputStorage { get; set; }
+
+        public void BuildStudentInformation()
+        {
+            this.OutputStorage = $"Student name : {this.StudentName} Study Hours : {this.StudyHoursPerWeek} Number of weeks completed : {this.StudyWeeksCompleted}";
+
+        }
+
+        public void DisplayStudentInformation()
+        {
+            Console.Write(this.OutputStorage);
+        }
+    }
+}
